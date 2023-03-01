d5c54237a4708aa163edce2fa09041fd643e2e43
AnwarA14 <122977277+AnwarA14@users.noreply.github.com>
2023-03-01 18:21:10 +0000

    Added first draft of models.py

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,41 +1,77 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-from django.contrib.auth.models import User
+# Create your models here.
 
-class Category(models.Model):
-    NAME_MAX_LENGTH = 128
+ratingChoices = [(1,1),(2,2),(3,3),(4,4),(5,5)]
 
-    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+#I left out the tags from stores and items as i'm unsure how to implement 5 different foreign keys user the tagID field
+
+class Users:
+    userID = models.BigAuto(unique=True,primary_key=True)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=32)
+    profilePicture = models.ImageField()
+    description = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    phoneNo = models.CharField(max_length=11)
     slug = models.SlugField(unique=True)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.username
 
-    class Meta:
-        verbose_name_plural = 'Categories'
+
+class Items:
+    itemID = models.BigAuto(unique=True,primary_key=True)
+    itemName = models.CharField(max_length=30) #Added this so i can return something with __str__
+    sellerID = models.ForeignKey(Sellers, on_delete=models.CASCADE)
+    userID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    sellerName = models.ForeignKey(Sellers, on_delete=models.CASCADE)
+    username = models.ForeignKey(Users, on_delete=models.CASCADE)
+    isDigital = models.BooleanField()
+    itemDescription = models.CharField(max_length=128)
+    itemImage = models.ImageField()
+    #NEED TO ADD RESTRICTED CHOICE FOR ALL CONDITIONS:
+    condition = models.CharField(max_length=10)
+    listedTime = models.DateTime()
+    sellTime = models.DateTime()
+    buyNowPrice = models.DecimalField()
 
     def __str__(self):
-        return self.name
+        return self.itemName
 
-class Page(models.Model):
-    TITLE_MAX_LENGTH = 128
-    URL_MAX_LENGTH = 200
 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=TITLE_MAX_LENGTH)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
+class Seller:
+    userID = models.ForeignKey(Users,on_delete=models.CASCADE,primary_key=True)
+    sellerName = models.CharField(max_length=30)
+    rating = models.IntegerField(max_length=1,choices=ratingChoices,default=1)
 
     def __str__(self):
-        return self.title
+        return self.sellerName
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
+class Tags:
+    tagID = models.AutoField(primary_key=true,unique=true)
+    tagName = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.user.username
+        return self.tagName
+
+
+class Bids:
+    bidID = models.BigAuto(unique=true,primary_key=true)
+    itemID = models.ForeignKey(Items,on_delete=models.CASCADE)
+    userID = models.ForeignKey(Users,on_delete=models.CASCADE)
+    bidTime = models.DateTime()
+    bidPrice = models.DecimalField()
+
+    def __str__(self):
+        return self.bidID
+
+class Stores:
+    storeID = models.BigAuto(unique=true,primary_key=true)
+    storeName = models.charField(max_length=30)
+    storeDescription = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.storeName
+
+
