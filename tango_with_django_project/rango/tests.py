6291a573adda61a2b559bb1235768c43d2e27194
Samir Mahmood <2659251M@student.gla.ac.uk>
2023-03-06 14:57:03 +0000

    Added 2 skeleton tests for Seller Model.

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -1,4 +1,15 @@
 from django.test import TestCase
-
-#aa
 # Create your tests here.
+class SellerMethodTests(TestCase):
+    def test_slug_line_creation(self):
+        seller = Seller(userID = x, sellerName = "Bob's Premier Collection", rating = 2)
+        seller.save()
+
+        self.assertEqual(category.slug, "bob's-premier-collection")
+
+    def test_rating_is_non_negative(self):
+        seller = Seller(userID = x, sellerName = "Bob's Premier Collection", rating = -1)
+        seller.save()
+        self.assertEqual(seller.views >= 0, True, f"Seller rating should be non negative. Instead rating of {seller.rating} was recieved")
+    
+    
