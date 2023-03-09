9bd6e7edf5c6674efb8b2b0d40335aa4cb600575
Samir Mahmood <2659251M@student.gla.ac.uk>
2023-03-09 20:37:34 +0000

    Population script tests passing. AssertionError needs to be fixed.

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -4,23 +4,24 @@ import importlib
 from django.test import TestCase
 from django.urls import reverse
 from django.conf import settings
-from django.contrib.auth.models import Users, Sellers, Stores, Items
-from django.core.exceptions import ObjectDoesNotExist
+from rango.models import Seller, Users, Bids, Items, Stores, Tags
+
+#from django.core.exceptions import ObjectDoesNotExist
 
 FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}==============={os.linesep}TEST FAILURE = ({os.linesep}==============={os.linesep})"
 FAILURE_FOOTER = f"{os.linesep}"
 
-class SellerMethodTests(TestCase):
-    def test_slug_line_creation(self):
-        seller = Sellers(userID = x, sellerName = "Bob's Premier Collection", rating = 2)
-        seller.save()
+# class SellerMethodTests(TestCase):
+#     def test_slug_line_creation(self):
+#         seller = Sellers(userID = x, sellerName = "Bob's Premier Collection", rating = 2)
+#         seller.save()
 
-        self.assertEqual(seller.slug, "bob's-premier-collection")
+#         self.assertEqual(seller.slug, "bob's-premier-collection")
 
-    def test_rating_is_non_negative(self):
-        seller = Sellers(userID = x, sellerName = "Bob's Premier Collection", rating = -1)
-        seller.save()
-        self.assertEqual(seller.views >= 0, True, f"Seller rating should be non negative. Instead rating of {seller.rating} was recieved")
+#     def test_rating_is_non_negative(self):
+#         seller = Sellers(userID = x, sellerName = "Bob's Premier Collection", rating = -1)
+#         seller.save()
+#         self.assertEqual(seller.views >= 0, True, f"Seller rating should be non negative. Instead rating of {seller.rating} was recieved")
 
 class PopulationScriptTests(TestCase):
     def setUp(self):
@@ -50,7 +51,7 @@ class PopulationScriptTests(TestCase):
     def test_sellers(self):
         #There should be 1 seller
         #CHANGE TEST LATER SO THAT 2 SELLERS ARE REQUIRED.
-        sellers = Sellers.objects.filter()
+        sellers = Seller.objects.filter()
         sellers_len = len(sellers)
         sellers_str = map(str, sellers)
 
@@ -58,10 +59,10 @@ class PopulationScriptTests(TestCase):
         self.assertTrue('marla' in sellers_str, f"{FAILURE_HEADER} The seller 'marla' was expected but not created.{FAILURE_FOOTER}")
         for seller in sellers:
             try:
-                Users.objects.get(seller.users)
-            except ObjectDoesNotExist:
-                raise ValueError(f"{FAILURE_HEADER} Seller does not exist in User table.{FAILURE_FOOTER}")
-                #NOT SURE IF THIS LINE ^ WORKS.
+                seller_name = seller.users
+                Users.objects.get(username = seller_name)
+            except Users.DoesNotExist:
+                raise ValueError(f"{FAILURE_HEADER} Seller '{seller_name}' does not exist in User table.{FAILURE_FOOTER}")
 
     def test_stores(self):
         #There should be 1 store
@@ -69,8 +70,9 @@ class PopulationScriptTests(TestCase):
         stores = Stores.objects.filter()
         stores_len = len(stores)
         stores_str = map(str, stores)
+        excepted_num_stores = 1
 
-        self.assertEquals(stores_len, 1, f"{FAILURE_HEADER} Expecting 1 store to be created, but found {stores_len}.{FAILURE_FOOTER}")
+        self.assertEquals(stores_len, excepted_num_stores, f"{FAILURE_HEADER} Expecting {excepted_num_stores} store to be created, but found {stores_len}.{FAILURE_FOOTER}")
         self.assertTrue("UMBRA_LLA" in stores_str, f"{FAILURE_HEADER} The store 'UMBRA_LLA' was expected but not created.{FAILURE_FOOTER}")
     
     def test_items(self):
@@ -86,28 +88,27 @@ class PopulationScriptTests(TestCase):
         self.assertTrue("umbrella" in items_str, f"{FAILURE_HEADER} The item 'umbrella' was expected but not found.{FAILURE_FOOTER}")
         self.assertTrue("football" in items_str, f"{FAILURE_HEADER} The item 'football' was expected but not found.{FAILURE_FOOTER}")
 
-        for expected_store in expected_stores:
-            store_items = expected_stores[expected_store]
-            self.check_store_items(expected_store, store_items)
+        for expected_store_name in expected_stores:
+            expected_store_items = expected_stores[expected_store_name]
+            self.check_store_items(expected_store_name, expected_store_items)
     
-    def check_store_items(self, expected_store, store_items):
-        store = Stores.objects.get(name=expected_store)
-        items = Items.objects.filter(name=store_items)
+    def check_store_items(self, expected_store_name, expected_store_items):
+        try:
+            expected_store = Stores.objects.get(storeName=expected_store_name)
+        except Stores.DoesNotExist:
+            raise ValueError(f"{FAILURE_HEADER} Expected {expected_store} store, but not created")
+        
+        actual_items = Items.objects.filter(storename = expected_store)
 
         #FIRST CHECK WE HAVE THE SAME !!!NUMBER!!! OF ITEMS IN EACH STORE AS EXPECTED
-        items_len = len(items)
-        store_items_len = len(store_items)
+        actual_items_len = len(actual_items)
+        expected_items_len = len(expected_store_items)
 
-        self.assertEquals(items_len, store_items_len, f"{FAILURE_HEADER} Expected {store_items_len} items in {len(store)}, but found {items_len}.{FAILURE_FOOTER}")
+        self.assertEquals(actual_items_len, expected_items_len)
 
         #NOW CHECK WE HAVE THE CORRECT ITEMS FOR EACH STORE
-        for expected_item in store_items:
-            try:
-                item = Items.objects.get(expected_item)
-            except ObjectDoesNotExist:
-                raise ValueError(f"{FAILURE_HEADER}The item {expected_item} in the {store} was not found")
-            
-            self.assertEquals(item.storename, store)
+        actual_items_names = set(map(str, actual_items))
+        self.assertEquals(actual_items_names, expected_store_items, f"expected the following items for the store {expected_store}: {expected_store_items}. But found {actual_items_names}")
 
 
 
