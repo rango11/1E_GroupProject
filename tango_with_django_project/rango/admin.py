0c80c9edebd42b64dedf9fc0a0a2af7dd15a1ae7
FKGSOFTWARE <fkgdesign@gmail.com>
2023-03-24 12:51:57 +0000

    deleted tango rip

--- tango_with_django_project/rango/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from rango.models import *
-
-admin.register(Users)
-admin.register(Sellers)
-admin.register(Items)
-admin.register(Bids)
-admin.register(Stores)
-admin.register(Tags)
